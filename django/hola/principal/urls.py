bc24e80336b5fc3f109876d56bde60a1af0ba18d
ErickGO228 <Desaster360@gmail.com>
2021-10-23 20:43:07 -0500

    Hola Tu agregado a views

--- django/hola/principal/urls.py
+++ django/hola/principal/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path("", views.holaDjango, name="holaDjango"),
-path("pepe", views.pepe, name="holaPepe")
+path("pepe", views.pepe, name="holaPepe"),
+path("<str:nombre>", views.views.holaTu, name="holaTu")
 ]
