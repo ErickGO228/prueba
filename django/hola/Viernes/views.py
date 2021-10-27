2d1f3b6f2c012d7b35b83fdf0e6e13a93cd4ee00
ErickGO228 <Desaster360@gmail.com>
2021-10-27 13:00:14 -0500

    Es viernes (actualizado)

--- django/hola/Viernes/views.py
+++ django/hola/Viernes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def paginaviernes(request):
-    return render(request, "Viernes/viernes.html")
+    return render(request, "Viernes/viernes.html", {"Esviernes": datetime.today().isoweekday() == 5})
