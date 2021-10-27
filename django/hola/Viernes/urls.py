8475a5b0795836e2faa48630b51b97618b2ddaac
ErickGO228 <Desaster360@gmail.com>
2021-10-27 13:31:06 -0500

    Es viernes V2

--- django/hola/Viernes/urls.py
+++ django/hola/Viernes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("viernes", views.paginaviernes, name= "indice"),
+    path("viernes2", views.paginaviernes2, name="paginaviernes2")
 ]
--- django/hola/Viernes/views.py
+++ django/hola/Viernes/views.py
@@ -4,3 +4,4 @@ from datetime import datetime
 # Create your views here.
 def paginaviernes(request):
     return render(request, "Viernes/viernes.html", {"Esviernes": datetime.today().isoweekday() == 5})
+
